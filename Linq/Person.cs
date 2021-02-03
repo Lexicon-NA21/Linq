7ef6cc0031440a1778dd6a7c3302ec9e4efb0638
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-02-03 10:35:09 +0100

    Add project files.

---
+++ Linq/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString() => $"Name: {Name} Age: {Age}";
+
+    }
+}
---
+++ Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>
+            {
+                new Person("Nisse", 20),
+                new Person("Pelle" ,65),
+                new Person("Olle" , 66),
+                new Person("Sara" , 54),
+                new Person("Ida" ,  36),
+                new Person("Fia",   45),
+                new Person("Sopia", 32),
+            };
+
+            var numbersArray = Enumerable.Range(1, 100).ToArray();
+            var numbersList = Enumerable.Range(1, 100).ToList();
+        }
+    }
+}
